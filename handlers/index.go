371c831c4daf7f9219a755e9eec0c22158f6e91c
Erika Takahara <erika.takahara@gmail.com>
2015-08-18 23:32:53 -0300

    Initial layout structure

--- handlers/index.go
+++ handlers/index.go
@@ -1,18 +1,13 @@
 package handlers
 
 import (
-    "appengine"
-    "appengine/user"
-    
     "html/template"
     "net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    t := template.Must(template.ParseFiles("webapp/index.html"))
-    c := appengine.NewContext(r)
-    u := user.Current(c)
-    err := t.Execute(w, u)
+    t := template.Must(template.ParseFiles("webapp/templates/base.html", "webapp/pages/index.html"))
+    err := t.Execute(w, nil)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
